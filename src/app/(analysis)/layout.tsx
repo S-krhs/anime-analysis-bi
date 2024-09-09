8cd0d48a76cf9ef813062591f54c96e48ff9a4b2
ORIETTA\shkur <yukko.hori.reset@gmail.com>
2024-09-09 13:12:38 +0900

    public/mockを利用しない

--- src/app/(analysis)/layout.tsx
+++ src/app/(analysis)/layout.tsx
@@ -1,6 +1,133 @@
 import "@/styles/globals.css"
 import { SideMenuPc, SideMenuSp } from '@/components/base/SideMenu'
 
+const list = [
+  {
+    "website_id": 1,
+    "website_name": "9anime",
+    "icon_url": "http://localhost:3000/mock/img/9anime.png",
+    "attributes": [
+      {
+        "attribute_id": 1,
+        "attribute_name": "favorites"
+      },
+      {
+        "attribute_id": 2,
+        "attribute_name": "users"
+      },
+      {
+        "attribute_id": 3,
+        "attribute_name": "nya-n"
+      }
+    ]
+  },
+  {
+    "website_id": 6,
+    "website_name": "danime",
+    "icon_url": "http://localhost:3000/mock/img/danime.png",
+    "attributes": [
+      {
+        "attribute_id": 1,
+        "attribute_name": "favorites"
+      },
+      {
+        "attribute_id": 2,
+        "attribute_name": "users"
+      }
+    ]
+  },
+  {
+    "website_id": 3,
+    "website_name": "AmazonPrimeVideoJPTV",
+    "icon_url": "http://localhost:3000/mock/img/amazon-tv.png",
+    "attributes": [
+      {
+        "attribute_id": 1,
+        "attribute_name": "favorites"
+      },
+      {
+        "attribute_id": 2,
+        "attribute_name": "users"
+      }
+    ]
+  },
+  {
+    "website_id": 10,
+    "website_name": "dummy",
+    "icon_url": "http://localhost:3000/mock/img/amazon-tv.png",
+    "attributes": [
+      {
+        "attribute_id": 1,
+        "attribute_name": "favorites"
+      },
+      {
+        "attribute_id": 2,
+        "attribute_name": "users"
+      }
+    ]
+  },
+  {
+    "website_id": 11,
+    "website_name": "dummy",
+    "icon_url": "http://localhost:3000/mock/img/amazon-tv.png",
+    "attributes": [
+      {
+        "attribute_id": 1,
+        "attribute_name": "favorites"
+      },
+      {
+        "attribute_id": 2,
+        "attribute_name": "users"
+      }
+    ]
+  },
+  {
+    "website_id": 12,
+    "website_name": "dummy",
+    "icon_url": "http://localhost:3000/mock/img/amazon-tv.png",
+    "attributes": [
+      {
+        "attribute_id": 1,
+        "attribute_name": "favorites"
+      },
+      {
+        "attribute_id": 2,
+        "attribute_name": "users"
+      }
+    ]
+  },
+  {
+    "website_id": 13,
+    "website_name": "dummy",
+    "icon_url": "http://localhost:3000/mock/img/amazon-tv.png",
+    "attributes": [
+      {
+        "attribute_id": 1,
+        "attribute_name": "favorites"
+      },
+      {
+        "attribute_id": 2,
+        "attribute_name": "users"
+      }
+    ]
+  },
+  {
+    "website_id": 14,
+    "website_name": "dummy",
+    "icon_url": "http://localhost:3000/mock/img/amazon-tv.png",
+    "attributes": [
+      {
+        "attribute_id": 1,
+        "attribute_name": "favorites"
+      },
+      {
+        "attribute_id": 2,
+        "attribute_name": "users"
+      }
+    ]
+  }
+]
+
 const AnalysisLayout = async ({
   children,
 }: Readonly<{
@@ -8,8 +135,7 @@ const AnalysisLayout = async ({
 }>) => {
   // todo -> supabase
   const url = process.env.BASE_URL + '/mock/api/site-list'
-  const websiteList = await fetch(url)
-    .then(res => res.json())
+  const websiteList = list
 
   return (
     <div className="flex flex-col h-screen bg-primary-50">
