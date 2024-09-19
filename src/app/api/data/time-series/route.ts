981d3ba458073118e0e00efbc61228c6d2d55f1d
ORIETTA\shkur <yukko.hori.reset@gmail.com>
2024-09-19 19:52:13 +0900

    ランダムデータ生成削除

--- src/app/api/data/time-series/route.ts
+++ src/app/api/data/time-series/route.ts
@@ -1,38 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { supabaseClient } from '@/lib/supabase'
 
-
-const titles = [
-    "Attack on Titan", "My Hero Academia", "One Piece", "Demon Slayer", "Jujutsu Kaisen",
-    "Naruto", "Dragon Ball", "Fullmetal Alchemist", "Sword Art Online", "Tokyo Revengers",
-    "Hunter x Hunter", "Bleach", "Death Note", "Re:Zero", "Black Clover",
-    "One Punch Man", "Your Lie in April", "Haikyuu!!", "Steins;Gate", "Code Geass",
-]
-const generateRandomData = () => {
-  const data = [];
-  const startDate = new Date(2024, 3, 1); // April 1, 2024
-  const endDate = new Date(2024, 3, 15); // April 15, 2024
-  
-  for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
-    const formattedDate = date.toISOString().split('T')[0]; // YYYY-MM-DD format
-
-    const dailyData = Array.from({ length: 20 }, () => ({
-      title: titles[Math.floor(Math.random() * titles.length)],
-      value: (Math.floor(Math.random() * 10000) + 1), // Random number between 1 and 10000
-    }));
-
-    data.push({ date: formattedDate, data: dailyData });
-  }
-  
-  return data;
-}
-// const timeSeriesData = generateRandomData()
-const titlesArray = titles.map(elem => {
-  return ({
-    title: elem
-  })
-})
-
 type GetScrapingData = {
   scraping_id: number
   job_id: number
